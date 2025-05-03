2a1cff4010c4d746617ba4f4ba94a3377877ce7e
Antzy <antzy.pantzy@gmail.com>
2025-05-03 11:09:01 +0530

    Server connection fix

--- Runtime/SocketConnector.cs
+++ Runtime/SocketConnector.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using SocketIOClient;
 using SocketIO.Core;
+using PimDeWitte.UnityMainThreadDispatcher;
 
 
 public class SocketConnector : MonoBehaviour
@@ -25,44 +26,98 @@ public class SocketConnector : MonoBehaviour
 
     private void ConnectToServer()
     {
-        client = new SocketIOClient.SocketIO(serverIp, new SocketIOOptions());
+        //client = new SocketIOClient.SocketIO(serverIp, new SocketIOOptions());
+        //client = new SocketIOClient.SocketIO(serverIp, new SocketIOOptions
+        //{
+        //    Query = new List<KeyValuePair<string, string>>
+        //    {
+        //        new KeyValuePair<string, string>("token", appKey),
+        //    }
+        //});
 
-        client = new SocketIOClient.SocketIO(serverIp, new SocketIOOptions
+        client = new SocketIOClient.SocketIO(serverIp, new SocketIOClient.SocketIOOptions
         {
-            Auth = new List<KeyValuePair<string, string>>
-    {
-        new KeyValuePair<string, string>("token", appKey),
-    }
+            Auth = new Dictionary<string, string>
+            {
+                { "token", appKey }
+            }
         });
 
         client.On("configHash", response =>
         {
-            // You can print the returned data first to decide what to do next.
-            // output: ["hi client"]
-
-            Debug.Log(response);
+            Debug.Log("configHash Response: " + response);
 
             string text = response.GetValue<string>();
 
-            Debug.Log("configHash: " + text);
-
-            // The socket.io server code looks like this:
-            // socket.emit('hi', 'hi client');
+            Debug.Log("configHash Text: " + text);
         });
+
+        client.On("appConfig", response =>
+        {
+            Debug.Log("appConfig Response: " + response);
+
+            string text = response.GetValue<string>();
+
+            Debug.Log("appConfig Text: " + text);
+        });
+
+        client.On("themeConfig", response =>
+        {
+            Debug.Log("themeConfig Response: " + response);
+
+            string text = response.GetValue<string>();
+
+            Debug.Log("themeConfig Text: " + text);
+        });
+
+        //client.On("transform", response =>
+        //{
+        //    try
+        //    {
+        //        Debug.Log("response: " + response);
+        //        string responseStr = response.ToString();
+        //        Debug.Log("responseStr: " + responseStr);
+
+        //        JSONNode parsedJson = SimpleJSON.JSON.Parse(responseStr)[0];
+
+        //        TestDTO dto = new TestDTO
+        //        {
+        //            name = parsedJson["name"],
+        //            rotation = parsedJson["rotation"].AsInt,
+        //            scale = parsedJson["scale"].AsFloat
+        //        };
+
+        //        // TestDTO dto = response.GetValue<TestDTO>(1);
+        //        Debug.Log(dto.name);
+        //        Debug.Log(dto.rotation);
+        //        Debug.Log(dto.scale);
+
+        //        UnityMainThreadDispatcher.Instance().Enqueue(ThisWillBeExecutedOnTheMainThread(dto));
+        //    }
+        //    catch (System.Exception e)
+        //    {
+        //        Debug.LogError("Transform error: " + e);
+        //    }
+        //});
 
         client.OnConnected += async (sender, e) =>
         {
 
             Debug.Log("Connected to server!");
-            // Emit a string
-            //await client.EmitAsync("hi", "socket.io");
-
-            // Emit a string and an object
-            //var dto = new TestDTO { Id = 123, Name = "bob" };
-            //await client.EmitAsync("register", "source", dto);
         };
         client.ConnectAsync();
     }
 
+    //public IEnumerator ThisWillBeExecutedOnTheMainThread(TestDTO dto)
+    //{
+    //    Debug.Log("Dto received in the main thread: " + dto.name);
+    //    testObj.transform.rotation = Quaternion.Euler(0, dto.rotation, 0);
+    //    testObj.transform.localScale = Vector3.one * dto.scale;
+    //    yield return null;
+    //}
 
+    void OnApplicationQuit()
+    {
+        client.DisconnectAsync();
+    }
 }
